bef507fbffcc109287646ce19973ff3b2f28cfc3
tburdittx <32482403+tburdittx@users.noreply.github.com>
2018-05-25 08:42:51 +0100

    Update JsGifs.js

--- JsGifs.js
+++ JsGifs.js
@@ -2,44 +2,24 @@ const API_KEY = "apiKey=8dHGvhpcAH8zaegGhpK1oJ1pKWpFSEU5"
 
 const URL = "https://api.giphy.com/v1"
 
-const RANDOM_END_POINT = "/gifs/random?"
-const TRENDING_END_POINT = "/gifs/trending"
+const TRENDING = "/gifs/trending?"
 
-function getGif() {
-    // we create a new instance of an HTTP request
-    var request = new XMLHttpRequest();
-    //we get the text that the user has typed in
-    searchTerm = document.getElementById('input-box').value;
-    // we set up the url endpoint we want to reach
-    var searchQuery = "&tag=" + searchTerm;
-    var requestUrl = URL + RANDOM_END_POINT + API_KEY + searchQuery
+function getGifs() {
+  // we create a new instance of an HTTP request
+  var request = new XMLHttpRequest();
+  // we set up the url endpoint we want to reach
+  var requestUrl = URL + TRENDING + API_KEY + "&limit=10"
+  
+  // we make the request
+  request.open('GET', requestUrl);
+  request.responseType = 'json';
+  request.send();
 
-    // we make the request
-    request.open('GET', requestUrl);
-    request.responseType = 'json';
-    request.send();
-
-    //we do something with the successful response
-    request.onload = function () {
-        var response = request.response;
-        var imageUrl = response.data.image_url;
-        document.getElementById('gif').src = imageUrl
+  //we do something with the successful response
+  request.onload = function() {
+    var response = request.response;
+    for(var i = 0; i < 10; i++) {
+      document.getElementById('gif' + (i + 1)).src = response.data[i].images.fixed_width.url;
     }
-
-}
-
-
-function GetGif1() {
-    var request = new XMLHttpRequest
-    var requestUrl = URL + TRENDING_END_POINT + API_KEY
-
-    request.open('GET', requestUrl);
-    request.responseType = 'json';
-    request.send();
-
-    request.onload = function () {
-        var response = request.response;
-        var imageUrl = response.data.image_url;
-        document.getElementById('First').src = imageUrl
-    }
+  }
 }
